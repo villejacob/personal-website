5426c01b32bc7154ae86651a7f816ffdd455015d
Jacob Ville <VilleLJacob@gmail.com>
2016-12-13 17:47:52 -0500

    Add fade animations to logo and about background

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ var height = $(window).height();
 $(window).on('scroll', function(){
 	window.requestAnimationFrame(function(){
 		var distance = $(window).scrollTop();
+		var fade = (distance/height);
 		$(".logo-container").css({
 			"transform": "translate3d(0," + distance * -0.3 + "px, 0)"
 		});
-		$(".film").css("opacity", -0.9*(distance/height - 1));
+		$(".film").css("opacity", 1 - fade);
+		$(".logo-container").css("opacity", -2.3*fade + 1);
 	});
 });
