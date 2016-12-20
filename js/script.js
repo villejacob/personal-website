5f678a693c57097294eaf0d921f34027d35318e6
Jacob Ville <VilleLJacob@gmail.com>
2016-12-20 18:18:07 -0500

    Start to add content to the about page

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(window).on('scroll', function(){
 		$(".logo-container").css({
 			"transform": "translate3d(0," + distance * -0.3 + "px, 0)"
 		});
-		$(".film").css("opacity", 1 - fade);
-		$(".logo-container").css("opacity", -2.3*fade + 1);
+		$(".about").css("opacity",  fade);
+		$(".logo-container").css("opacity", -2.6*fade + 1);
 	});
 });
